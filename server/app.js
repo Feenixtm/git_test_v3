b375f1e33ff7e5b5546969d9b5ca39a438ade1b7
feenixtm <177155245+Feenixtm@users.noreply.github.com>
2026-08-03 15:35:08 -0700

    Added 1st Post Request!

--- server/app.js
+++ server/app.js
@@ -1,13 +1,28 @@
 import express from "express"
+import cors from "cors"; // Allows you to perform a fetch request from a different port.
+
 const app = express();
 
-// test 
+app.use(cors()); // Allows frontend requests from different port numbers
+app.use(express.json()); // 'Parses incoming JSON request bodies'
 
 app.get("/", (req, res) => {
-    res.send("Hello world!");
+    // res.send("Hello world!");
+    res.json({
+        message: "Fetch Request Successful!"
+    })
+});
+
+app.post("/sign-up", (req, res) => {
+    const { username, password } = req.body;
+
+    res.status(201).json({
+        message: "Sign-up submission received",
+        receviedData: { username, password }
+    })
 })
 
-const PORT = 8080;
+const PORT = 5050;
 
 app.listen(PORT, () => {
     console.log("Listening to PORT: " + PORT);
