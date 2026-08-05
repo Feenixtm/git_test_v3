eded32e645913430409349db4517786cf9abef23
feenixtm <177155245+Feenixtm@users.noreply.github.com>
2026-08-04 18:27:42 -0700

    Imported Prisma to app.js

--- server/app.js
+++ server/app.js
@@ -1,16 +1,34 @@
 import express from "express"
 import cors from "cors"; // Allows you to perform a fetch request from a different port.
 
+// Importing Prisma FROM /lib/prisma.js
+import { prisma } from "./lib/prisma.js"
+
+// -------------------------------
+
 const app = express();
 
 app.use(cors()); // Allows frontend requests from different port numbers
 app.use(express.json()); // 'Parses incoming JSON request bodies'
 
-app.get("/", (req, res) => {
+
+app.get("/",  (req, res) => {
     // res.send("Hello world!");
     res.json({
         message: "Fetch Request Successful!"
     })
+});
+
+// Need to turn function to async when using prisma
+// TESTING
+app.get("/all-users", async (req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        // const users = { message: "Here are all the users..."}
+        res.json(users);
+    } catch (error) {
+        res.status(400).json({error: `Database retrieval process failed...`})
+    }
 });
 
 app.post("/sign-up", (req, res) => {
